7b14937f2235a7382e692ceadac33d9efad2a772
Labiba Ibnat <labiba.ssp@gmail.com>
2020-09-15 23:44:35 +0600

    card component using grid in material-ui

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import {
+  // Button,
+  // Typography,
+  Grid,
+} from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import Header from "./Header";
+import Content from "./Content";
+
+const useStyles = makeStyles({
+  heyStyle: {
+    fontStyle: "oblique",
+    color: "red",
+  },
+});
 
 function App() {
+  const classes = useStyles();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <Typography className={classes.heyStyle} variant="h3">Hey it's me LABIBA</Typography>
+      <Button color="secondary" variant="outlined">Hello World</Button> */}
+      <Grid container direction="column">
+        <Grid item>
+          <Header />
+        </Grid>
+        <Grid item container>
+          <Grid item xs={0} sm={2} />
+          <Grid xs={12} sm={8}>
+            <Content/>
+          </Grid>
+          <Grid item xs={0} sm={2} />
+        </Grid>
+      </Grid>
     </div>
   );
 }
