b8b4ebade95c28b688248ee4809d8c17087d2329
Labiba Ibnat <labiba.ssp@gmail.com>
2020-11-09 19:22:26 +0600

    Updates

---
+++ precache-manifest.8932d5c77ef015c6dbff614ae6b02acc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3b432b4c9903550c1831b5039877a23",
+    "url": "/pokedeck/index.html"
+  },
+  {
+    "revision": "142d2647d7c71e5acd1e",
+    "url": "/pokedeck/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0599eb2f8870c6c0d1fc",
+    "url": "/pokedeck/static/js/2.0ad6b5af.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/pokedeck/static/js/2.0ad6b5af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "142d2647d7c71e5acd1e",
+    "url": "/pokedeck/static/js/main.7c6e9b87.chunk.js"
+  },
+  {
+    "revision": "cbaeef840c3e744dd6e1",
+    "url": "/pokedeck/static/js/runtime-main.930f5d6b.js"
+  }
+]);
